cc873c079889d1110970212af58af68b55590db4
Diogo Willian <diogowillian499@gmail.com>
2026-02-22 17:02:48 -0300

    desenhar a partir do tilemap

--- source/assets/model/world.py
+++ source/assets/model/world.py
@@ -3,6 +3,15 @@ import pyxel
 class World:
     TILE_SIZE = 8
 
+    HEIGHT = TILE_SIZE * 2
+    WIDTH = TILE_SIZE * 8
+
+    # X and Y
+    VAZIO = (0, 0)
+    CHAO_1 = (1, 0)
+    CHAO_2 = (1, 1)
+    FAIXA = (0, 1)
+
     def __init__(self):
         # Gravidade
         self.gravity = 1.25
@@ -11,6 +20,21 @@ class World:
         self.chao_x = 0
         self.chao_y = 200 - (3 * self.TILE_SIZE)
         self.chao_width = 64 * self.TILE_SIZE
+
+        self.tilemap = pyxel.tilemaps[0]
+        self.world_map = []
+        
+        for y in range(self.HEIGHT):
+            self.world_map.append([])
+            for x in range(self.WIDTH):
+                if self.tilemap.pget(x, y) == self.CHAO_1:
+                    self.world_map[y].append(self.CHAO_1)
+                elif self.tilemap.pget(x, y) == self.CHAO_2:
+                    self.world_map[y].append(self.CHAO_2)
+                elif self.tilemap.pget(x, y) == self.FAIXA:
+                    self.world_map[y].append(self.FAIXA)
+                else:
+                    self.world_map[y].append(self.VAZIO)
     
     def update(self):
         return
@@ -25,3 +49,17 @@ class World:
             self.chao_width,
             3 * self.TILE_SIZE
         )
+
+        for y in range(self.HEIGHT):
+            for x in range(self.WIDTH):
+                world_item = self.world_map[y][x]
+                
+                pyxel.blt(
+                    x * self.TILE_SIZE,
+                    y * self.TILE_SIZE,
+                    0,
+                    world_item[0]  * self.TILE_SIZE,
+                    world_item[1]  * self.TILE_SIZE,
+                    self.TILE_SIZE,
+                    self.TILE_SIZE
+                )
