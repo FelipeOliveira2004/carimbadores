578296791b69a74e99d3a7aff8c737c7952506b0
Diogo Willian <diogowillian499@gmail.com>
2026-02-22 19:17:31 -0300

    Remove __pycache__ do repositório e fazendo o player ter colisão baseado no tilemap

--- source/assets/model/player.py
+++ source/assets/model/player.py
@@ -26,54 +26,43 @@ class Player:
         self.moving = False
 
         # Pulo
-        self.jump_force = -10
+        self.jump_force = -9.9
         self.on_ground = False
 
         # Gravidade Velocidade
         self.vel_y = 0
+        self.vel_x = 0
 
         # Referência ao mundo (instância passada de main)
         self.world = world
     
     def update(self):
-        # Gravidade
-        self.vel_y += self.world.gravity
-        self.player_y += self.vel_y
-
-        # Movimento
+        # -------- MOVIMENTO HORIZONTAL --------
+        self.vel_x = 0
         self.moving = False
-
-        # Colisão Chão
-        ground = self.world.chao_y - self.HEIGHT  # Altura do personagem
-
-        # esta_no_chao_L = (self.player_x + self.WIDTH) >= self.world.chao_x
-        # esta_no_chao_R = self.player_x <= (self.world.chao_x + self.world.chao_width)
-        if (self.player_x + self.WIDTH) >= self.world.chao_x and self.player_x <= (self.world.chao_x + self.world.chao_width):
-            esta_no_chao = True
-        else:
-            esta_no_chao = False
-            self.on_ground = False
-
-        if self.player_y >= ground and esta_no_chao:
-            self.player_y = ground
-            self.vel_y = 0
-            self.on_ground = True
-
-        ## Personagem
-        # Movimentação
+        
         if pyxel.btn(pyxel.KEY_D) and self.player_x <= ((self.world.chao_width + self.world.chao_x) - self.WIDTH):
-            self.player_x += 2
+            self.vel_x = 2
             self.facing = 1
             self.moving = True
-        
-        if pyxel.btn(pyxel.KEY_A) and self.player_x >= 0:
-            self.player_x -= 2
+        if pyxel.btn(pyxel.KEY_A) and self.player_x > 0:
+            self.vel_x = -2
             self.facing = -1
             self.moving = True
-        
+
+        self.player_x += self.vel_x
+        self.resolve_horizontal_collision()
+
+        # -------- GRAVIDADE --------
+        self.vel_y += self.world.gravity
+        self.player_y += self.vel_y
+        self.on_ground = False
+
+        self.resolve_vertical_collision()
+
+        # -------- PULO --------
         if pyxel.btnp(pyxel.KEY_W) and self.on_ground:
             self.vel_y = self.jump_force
-            self.on_ground = False
 
         # Animação
         if self.moving:
@@ -113,3 +102,60 @@ class Player:
             self.HEIGHT, 
             0
         )
+
+    def is_solid(self, tile):
+        return tile in (self.world.CHAO_1, self.world.CHAO_2, self.world.FAIXA)
+    
+    def is_solid_at_pixel(self, x, y):
+        tile_x = int(x // self.world.TILE_SIZE)
+        tile_y = int(y // self.world.TILE_SIZE)
+
+        if tile_x < 0 or tile_x >= len(self.world.world_map[0]):
+            return False
+        if tile_y < 0 or tile_y >= len(self.world.world_map):
+            return False
+
+        return self.is_solid(self.world.world_map[tile_y][tile_x])
+    
+    def resolve_horizontal_collision(self):
+        if self.vel_x > 0:  # Indo para direita
+            right = self.player_x + self.WIDTH
+            top = self.player_y
+            bottom = self.player_y + self.HEIGHT - 1
+
+            if (self.is_solid_at_pixel(right, top) or self.is_solid_at_pixel(right, bottom)):
+                tile_x = right // self.world.TILE_SIZE
+                self.player_x = tile_x * self.world.TILE_SIZE - self.WIDTH
+
+
+        elif self.vel_x < 0:  # Indo para esquerda
+            left = self.player_x
+            top = self.player_y
+            bottom = self.player_y + self.HEIGHT - 1
+
+            if (self.is_solid_at_pixel(left, top) or self.is_solid_at_pixel(left, bottom)):
+                tile_x = left // self.world.TILE_SIZE
+                self.player_x = (tile_x + 1) * self.world.TILE_SIZE
+
+    def resolve_vertical_collision(self):
+        if self.vel_y > 0:  # Caindo
+            bottom = self.player_y + self.HEIGHT - 1
+            left = self.player_x
+            right = self.player_x + self.WIDTH - 1
+
+            if (self.is_solid_at_pixel(left, bottom) or self.is_solid_at_pixel(right, bottom)):
+                tile_y = bottom // self.world.TILE_SIZE
+                self.player_y = tile_y * self.world.TILE_SIZE - self.HEIGHT
+                self.vel_y = 0
+                self.on_ground = True
+
+
+        elif self.vel_y < 0:  # Subindo (batendo cabeça)
+            top = self.player_y
+            left = self.player_x
+            right = self.player_x + self.WIDTH - 1
+
+            if (self.is_solid_at_pixel(left, top) or self.is_solid_at_pixel(right, top)):
+                tile_y = top // self.world.TILE_SIZE
+                self.player_y = (tile_y + 1) * self.world.TILE_SIZE
+                self.vel_y = 0
